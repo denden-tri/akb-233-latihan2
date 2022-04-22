d8921b5609ec1afa664f163589d8bdc91134a89e
denden-tri <83654878+denden-tri@users.noreply.github.com>
2022-04-22 22:22:23 +0700

    menambahkan identitas pada MainActivity class

--- app/src/main/java/com/denztri/bloodshare233/MainActivity.java
+++ app/src/main/java/com/denztri/bloodshare233/MainActivity.java
@@ -8,6 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.denztri.bloodshare233.ui.register.RegisterActivity;
 import com.google.android.material.button.MaterialButton;
 
+// NIM                  : 10119233
+// Nama                 : Denden Triana
+// Kelas                : IF6
+// Tanggal Pengerjaan   : 22-04-2022
+// Deskripsi Pekerjaan  : 1. Pengerjaan button onClick tombol register (R.id.login_register_button),
+//                           agar berpindah ke RegisterActivity saat di klik dari MainActivity.
+//                        2. Pengerjaan menampilkan list/array string untuk menjadi pilihan di
+//                           component Spinner di RegisterActivity
+//                        3. Pengerjaan button onClick tombol back pada RegisterActivity agar dapat
+//                           kembali ke MainActivity
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
